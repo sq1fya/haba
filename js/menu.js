1dd03fce7cb963a989420c5dcb815daf76684589
sq1fya <rgpszczecin@gmail.com>
2018-11-25 20:38:13 +0100

    Fix navigation menu on scroll

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     document.getElementById("main_menu").style.padding = "5px 30px";
-    document.getElementById("slide_menu").style.padding = "5px 30px";
+    document.getElementById("slide_menu").style.padding = "5px 35px";
   } else {
     document.getElementById("main_menu").style.padding = "20px 30px";
     document.getElementById("slide_menu").style.padding = "22px 35px";
